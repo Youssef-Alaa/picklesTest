4f4c880ce9908c1927e390c01eafd5f8535328cd
Youssef-Alaa <eng.youssefalaa@gmail.com>
2020-11-19 18:09:05 +0200

    deployed UniDai Strategy

--- tests/controller.test.js
+++ tests/controller.test.js
@@ -1,23 +1,27 @@
+/* eslint-disable no-undef */
 const { ethers } = require("ethers");
 const chalk = require("chalk");
-const uniswap = require("@studydefi/money-legos/uniswap");
-const erc20 = require("@studydefi/money-legos/erc20");
-
+// const uniswap = require("@studydefi/money-legos/uniswap");
+// const erc20 = require("@studydefi/money-legos/erc20");
+const { constants } = require('@openzeppelin/test-helpers');
+const { ZERO_ADDRESS } = constants;
 const { ABIS, BYTECODE, KEYS } = require("../scripts/constants");
 const { deployContract, provider, wallets } = require("../scripts/common");
 
 jest.setTimeout(100000);
+let wallet;
+let Controller;
+let psUNIDAI;
+let StrategyUniEthDaiLpV4;
+
+const tempGov = wallets[0].address;
+const tempTimelock = wallets[0].address;
+const strategist = wallets[1].address;
+const devfund = "0x2fee17F575fa65C06F10eA2e63DBBc50730F145D";
+const treasury = "0x066419EaEf5DE53cc5da0d8702b990c5bc7D1AB3";
+const uniEthDai = "0xA478c2975Ab1Ea89e8196811F51A7B7Ade33eB11";
 
 describe("Test ControllerV4 Contract", () => {
-  let wallet;
-  let Controller;
-
-  const tempGov = wallets[0].address;
-  const tempTimelock = wallets[0].address;
-  const strategist = wallets[1].address;
-  const devfund = "0x2fee17F575fa65C06F10eA2e63DBBc50730F145D";
-  const treasury = "0x066419EaEf5DE53cc5da0d8702b990c5bc7D1AB3";
-
   beforeAll(async () => {
     wallet = await wallets[0];
     Controller = await deployContract({
@@ -28,49 +32,90 @@ describe("Test ControllerV4 Contract", () => {
     });
   });
 
-  test("check governance address", async () => {
-    const governance = await Controller.governance();
-    console.log(governance);
-    expect(governance).toBe(tempGov);
+  test("valid governance address", async () => {
+    const contGovernance = await Controller.governance();
+    expect(contGovernance).toBe(tempGov);
   });
 
-  // test("buy DAI from Uniswap", async () => {
-  //   // 1. instantiate contracts
-  //   const daiContract = new ethers.Contract(
-  //     erc20.dai.address,
-  //     erc20.dai.abi,
-  //     wallet,
-  //   );
-  //   const uniswapFactoryContract = new ethers.Contract(
-  //     uniswap.factory.address,
-  //     uniswap.factory.abi,
-  //     wallet,
-  //   );
-  //   const daiExchangeAddress = await uniswapFactoryContract.getExchange(
-  //     erc20.dai.address,
-  //   );
-  //   const daiExchangeContract = new ethers.Contract(
-  //     daiExchangeAddress,
-  //     uniswap.exchange.abi,
-  //     wallet,
-  //   );
+  test("valid strategist address", async () => {
+    const contStrategist = await Controller.strategist();
+    expect(contStrategist).toBe(strategist);
+  });
 
-  //   // 2. do the actual swapping
-  //   await daiExchangeContract.ethToTokenSwapInput(
-  //     1, // min amount of token retrieved
-  //     2525644800, // random timestamp in the future (year 2050)
-  //     {
-  //       gasLimit: 4000000,
-  //       value: ethers.utils.parseEther("5"),
-  //     },
-  //   );
+  test("valid timeLock address", async () => {
+    const contTimeLock = await Controller.timelock();
+    expect(contTimeLock).toBe(tempTimelock);
+  });
 
-  //   // util function
-  //   const fromWei = (x) => ethers.utils.formatUnits(x, 18);
+  test("valid devFund address", async () => {
+    const contDEvFund = await Controller.devfund();
+    expect(contDEvFund).toBe(devfund);
+  });
 
-  //   // 3. check DAI balance
-  //   const daiBalanceWei = await daiContract.balanceOf(wallet.address);
-  //   const daiBalance = parseFloat(fromWei(daiBalanceWei));
-  //   expect(daiBalance).toBeGreaterThan(0);
-  // });
+  test("valid treasury address", async () => {
+    const contTreasury = await Controller.treasury();
+    expect(contTreasury).toBe(treasury);
+  });
+
+  const setJarApproveAndSetStrategy = async (jar, strat) => {
+    const gasPrice = await wallet.provider.getGasPrice();
+    const fastGasPrice = gasPrice
+      .mul(ethers.BigNumber.from(125))
+      .div(ethers.BigNumber.from(100));
+  
+    const want = await strat.want();
+
+    let tx = await Controller.setJar(want, jar.address, {
+      gasLimit: 1000000,
+      gasPrice: fastGasPrice,
+    });
+    await tx.wait();
+  
+    tx = await Controller.approveStrategy(want, strat.address, {
+      gasLimit: 1000000,
+      gasPrice: fastGasPrice,
+    });
+    await tx.wait();
+
+    tx = await Controller.setStrategy(want, strat.address, {
+      gasLimit: 1000000,
+      gasPrice: fastGasPrice,
+    });
+    await tx.wait();
+  
+    // Make sure jar is set
+    const controllerJar = await Controller.jars(want);
+    if (controllerJar.toLowerCase() !== jar.address.toLowerCase()) {
+      console.log(
+        chalk.red(`Invalid jar, expected: ${jar.address}, got ${controllerJar}`)
+      );
+    }
+  
+    const controllerStrat = await Controller.strategies(want);
+    if (controllerStrat.toLowerCase() !== strat.address.toLowerCase()) {
+      console.log(
+        chalk.red(
+          `Invalid strategy, expected: ${strat.address}, got ${controllerStrat}`
+        )
+      );
+    }
+  };
+
+  test("Deploy psUNIDAI + its Strategy AND Approve them", async () => {
+    psUNIDAI = await deployContract({
+      name: "psUNIDAI",
+      abi: ABIS.Pickle.PickleJar,
+      bytecode: BYTECODE.Pickle.PickleJar,
+      args: [uniEthDai, tempGov, tempTimelock, Controller.address]
+    });
+    StrategyUniEthDaiLpV4 = await deployContract({
+      name: "StrategyUniEthDaiLpV4",
+      abi: ABIS.Pickle.Strategies.StrategyUniEthDaiLpV4,
+      bytecode: BYTECODE.Pickle.Strategies.StrategyUniEthDaiLpV4,
+      args: [tempGov, strategist, Controller.address, tempTimelock]
+    });
+    await setJarApproveAndSetStrategy(psUNIDAI, StrategyUniEthDaiLpV4);
+    expect(psUNIDAI.address).not.toBe(ZERO_ADDRESS);
+    expect(StrategyUniEthDaiLpV4.address).not.toBe(ZERO_ADDRESS);
+  });
 });
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,74 +1,74 @@
-const { ethers } = require("ethers");
+// const { ethers } = require("ethers");
 
-const PRIV_KEY = "";
+// const PRIV_KEY = "";
 
-const startChain = async () => {
-  const provider = new ethers.providers.JsonRpcProvider();
-  const wallet = new ethers.Wallet(PRIV_KEY, provider);
+// const startChain = async () => {
+//   const provider = new ethers.providers.JsonRpcProvider();
+//   const wallet = new ethers.Wallet(PRIV_KEY, provider);
 
-  return wallet;
-};
+//   return wallet;
+// };
 
-jest.setTimeout(100000);
-const uniswap = require("@studydefi/money-legos/uniswap");
-const erc20 = require("@studydefi/money-legos/erc20");
+// jest.setTimeout(100000);
+// const uniswap = require("@studydefi/money-legos/uniswap");
+// const erc20 = require("@studydefi/money-legos/erc20");
 
-describe("do some tests", () => {
-  let wallet;
+// describe("do some tests", () => {
+//   let wallet;
 
-  beforeAll(async () => {
-    wallet = await startChain();
-  });
+//   beforeAll(async () => {
+//     wallet = await startChain();
+//   });
 
-  test("initial DAI balance of 0", async () => {
-    const daiContract = new ethers.Contract(
-      erc20.dai.address,
-      erc20.dai.abi,
-      wallet,
-    );
-    console.log(erc20.dai.address);
-    const daiBalanceWei = await daiContract.balanceOf(wallet.address)
-    const daiBalance = ethers.utils.formatUnits(daiBalanceWei, 18)
-    expect(parseFloat(daiBalance)).toBe(0)
-  });
+//   test("initial DAI balance of 0", async () => {
+//     const daiContract = new ethers.Contract(
+//       erc20.dai.address,
+//       erc20.dai.abi,
+//       wallet,
+//     );
+//     console.log(erc20.dai.address);
+//     const daiBalanceWei = await daiContract.balanceOf(wallet.address)
+//     const daiBalance = ethers.utils.formatUnits(daiBalanceWei, 18)
+//     expect(parseFloat(daiBalance)).toBe(0)
+//   });
 
-  test("buy DAI from Uniswap", async () => {
-    // 1. instantiate contracts
-    const daiContract = new ethers.Contract(
-      erc20.dai.address,
-      erc20.dai.abi,
-      wallet,
-    );
-    const uniswapFactoryContract = new ethers.Contract(
-      uniswap.factory.address,
-      uniswap.factory.abi,
-      wallet,
-    );
-    const daiExchangeAddress = await uniswapFactoryContract.getExchange(
-      erc20.dai.address,
-    );
-    const daiExchangeContract = new ethers.Contract(
-      daiExchangeAddress,
-      uniswap.exchange.abi,
-      wallet,
-    );
+//   test("buy DAI from Uniswap", async () => {
+//     // 1. instantiate contracts
+//     const daiContract = new ethers.Contract(
+//       erc20.dai.address,
+//       erc20.dai.abi,
+//       wallet,
+//     );
+//     const uniswapFactoryContract = new ethers.Contract(
+//       uniswap.factory.address,
+//       uniswap.factory.abi,
+//       wallet,
+//     );
+//     const daiExchangeAddress = await uniswapFactoryContract.getExchange(
+//       erc20.dai.address,
+//     );
+//     const daiExchangeContract = new ethers.Contract(
+//       daiExchangeAddress,
+//       uniswap.exchange.abi,
+//       wallet,
+//     );
 
-    // 2. do the actual swapping
-    await daiExchangeContract.ethToTokenSwapInput(
-      1, // min amount of token retrieved
-      2525644800, // random timestamp in the future (year 2050)
-      {
-        gasLimit: 4000000,
-        value: ethers.utils.parseEther("5"),
-      },
-    );
+//     // 2. do the actual swapping
+//     await daiExchangeContract.ethToTokenSwapInput(
+//       1, // min amount of token retrieved
+//       2525644800, // random timestamp in the future (year 2050)
+//       {
+//         gasLimit: 4000000,
+//         value: ethers.utils.parseEther("5"),
+//       },
+//     );
 
-    // util function
-    const fromWei = (x) => ethers.utils.formatUnits(x, 18);
+//     // util function
+//     const fromWei = (x) => ethers.utils.formatUnits(x, 18);
 
-    // 3. check DAI balance
-    const daiBalanceWei = await daiContract.balanceOf(wallet.address);
-    const daiBalance = parseFloat(fromWei(daiBalanceWei));
-    expect(daiBalance).toBeGreaterThan(0);
-  });
-});
+//     // 3. check DAI balance
+//     const daiBalanceWei = await daiContract.balanceOf(wallet.address);
+//     const daiBalance = parseFloat(fromWei(daiBalanceWei));
+//     expect(daiBalance).toBeGreaterThan(0);
+//   });
+// });
